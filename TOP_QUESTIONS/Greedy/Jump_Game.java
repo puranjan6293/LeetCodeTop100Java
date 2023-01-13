54d6c2798080147e835bb506f6636a711d2e7dd5
puranjan <mallik.puranjan@gmail.com>
2023-01-13 19:43:14 +0530

    Jump game

---
+++ TOP_QUESTIONS/Greedy/Jump_Game.java
@@ -0,0 +1,26 @@
+package TOP_QUESTIONS.Greedy;
+
+public class Jump_Game {
+    public boolean canJump(int[] nums) {
+        //Approach: Using Greedy Algorithm, T.C: O(n)
+        //Initialize the variables
+        int lastIndex = nums.length-1;
+        int maxReach = 0;
+
+        //Iterate through the array
+        for(int i=0;i<nums.length;i++){
+            //check if the curr index can be reached
+            if(i>maxReach){
+                return false;
+            }
+            //update the maximum reachable index
+            maxReach = Math.max(maxReach, i+nums[i]);
+            // if the maximum reachable index is greater than or equal to the last index, we can reach the last index
+            if(maxReach>=lastIndex){
+                return true;
+            }
+        }
+        //if loop complete, we can't reach the last index
+        return false;
+    }
+}
