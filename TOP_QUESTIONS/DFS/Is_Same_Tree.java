b35929d60cfd4bfd12d5f745f7160a7f1501cf14
puranjan <mallik.puranjan@gmail.com>
2023-01-21 11:21:42 +0530

    is same tree

---
+++ TOP_QUESTIONS/DFS/Is_Same_Tree.java
@@ -0,0 +1,16 @@
+package TOP_QUESTIONS.DFS;
+
+public class Is_Same_Tree {
+    /*
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //if both are null
+        if(p==null && q==null) return true;
+        
+        //if one of them is null
+        if(p==null || q==null || p.val!=q.val) return false;
+
+        //call recursion for left and right of the trees
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+     */
+}
