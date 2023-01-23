25c84bd9f4aa78bc38914bcc0f3edb2d14fea4a9
puranjan <mallik.puranjan@gmail.com>
2023-01-23 23:38:13 +0530

    binary tree zigzag level order traversal

---
+++ TOP_QUESTIONS/BFS/Tree_ZigZag_Level_traversal.java
@@ -0,0 +1,43 @@
+package TOP_QUESTIONS.BFS;
+
+public class Tree_ZigZag_Level_traversal {
+    /*
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        //Edge case
+        if(root==null) return ans;
+        //make a queue for storing the levels
+        Queue<TreeNode> q = new LinkedList<>();
+        //add root to queue
+        q.add(root);
+        
+        int oddEven = 0; //for checking the curr level is odd or even
+        //traverse
+        while(!q.isEmpty()){
+            int size = q.size();
+            //for storing the values of each level
+            List<Integer> level = new ArrayList<>();
+            //loop for zigzag or odd even level
+            for(int i=0;i<size;i++){
+                TreeNode node = q.poll();
+                //conditions
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
+                //add values to level
+                level.add(node.val);
+            }
+            //condition for Zigzag or oddeven
+            if(oddEven%2!=0){
+                Collections.reverse(level);
+            }
+            ans.add(level);
+            oddEven++;
+        }
+        return ans;
+    }
+     */
+}
