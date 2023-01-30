6e4629e49e7837f597cb3d65ff222bdbb530a008
puranjan <mallik.puranjan@gmail.com>
2023-01-30 23:34:39 +0530

    Merge two soeted lists

---
+++ TOP_QUESTIONS/LinkedList/Merge_Two_SortedLists.java
@@ -0,0 +1,24 @@
+package TOP_QUESTIONS.LinkedList;
+
+public class Merge_Two_SortedLists {
+    /*
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //Approach: T.C: O(n)
+        //base case
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+
+        //case 1, if value of list 1 is less than value of list 2
+        //add all values of list 2 in list1
+        if(list1.val<list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        ////add all values of list 2 in list1
+        else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+     */
+}
