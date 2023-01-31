29075f0cc0b9099f9e0fffc1bacd6a158e55440c
puranjan <mallik.puranjan@gmail.com>
2023-01-31 23:37:32 +0530

    Swap Nodes in pairs

---
+++ TOP_QUESTIONS/LinkedList/Swap_Nodes_in_Pairs.java
@@ -0,0 +1,24 @@
+package TOP_QUESTIONS.LinkedList;
+
+public class Swap_Nodes_in_Pairs {
+    /*
+    public ListNode swapPairs(ListNode head) {
+        //Approach: Using Recursion
+
+        //Edge case
+        if(head==null || head.next==null){
+            return head;
+        }
+
+        //initialize new node as head.next
+        ListNode newNode = head.next;
+        //call recursion
+        head.next = swapPairs(head.next.next);
+        //make new head
+        newNode.next = head;
+
+        //return new Node
+        return newNode;
+    }
+     */
+}
