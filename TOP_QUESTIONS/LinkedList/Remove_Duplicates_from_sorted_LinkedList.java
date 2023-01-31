cc270305007346243c1f4395e52be18e2855f7af
puranjan <mallik.puranjan@gmail.com>
2023-01-31 23:49:46 +0530

    Remove Duplicates from sorted LinkedList

---
+++ TOP_QUESTIONS/LinkedList/Remove_Duplicates_from_sorted_LinkedList.java
@@ -0,0 +1,26 @@
+package TOP_QUESTIONS.LinkedList;
+
+public class Remove_Duplicates_from_sorted_LinkedList {
+    /*
+    public ListNode deleteDuplicates(ListNode head) {
+        //Edge case 
+        if(head==null || head.next==null){
+            return head;
+        }
+
+        //initialize curr list
+        ListNode curr = head;
+        //traverse through the Linkedlist
+        while(curr.next!=null){
+            //if same values
+            if(curr.next.val==curr.val){
+                //connect curr.next to curr.next.next
+                curr.next = curr.next.next;
+            }
+            //else move one step forward
+            else curr = curr.next;
+        }
+        return head;
+    }
+     */
+}
