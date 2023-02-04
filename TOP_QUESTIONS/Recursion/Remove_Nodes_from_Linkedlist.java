d71f797bc1f4004155e967446e19ebd9c38074f8
puranjan <mallik.puranjan@gmail.com>
2023-02-04 22:25:26 +0530

    Remove nodes from linked list

---
+++ TOP_QUESTIONS/Recursion/Remove_Nodes_from_Linkedlist.java
@@ -0,0 +1,20 @@
+package TOP_QUESTIONS.Recursion;
+
+public class Remove_Nodes_from_Linkedlist {
+    /*
+    public ListNode removeNodes(ListNode head) {
+        //Approach, Using Recurrsion, T.C: O(n)
+
+        //edge case
+        if(head.next==null) return head;
+
+        //traverse
+        head.next = removeNodes(head.next);
+        //condition
+        if(head.next.val>head.val) return head.next;
+
+        //return head
+        return head;
+    }
+     */
+}
