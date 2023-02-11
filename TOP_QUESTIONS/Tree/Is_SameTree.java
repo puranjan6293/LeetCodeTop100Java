f3d2919f0087362ca4e9c6b5c8525df539cf5673
puranjan <mallik.puranjan@gmail.com>
2023-02-11 23:38:42 +0530

    is same tree

---
+++ TOP_QUESTIONS/Tree/Is_SameTree.java
@@ -0,0 +1,16 @@
+package TOP_QUESTIONS.Tree;
+
+public class Is_SameTree {
+    /*
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //Approach, Using recursion, T.C: O(n), n is the height of the tree
+        //case1: if both are null
+        if(p==null && q==null) return true;
+        //case2: if one of them is null and values are not same
+        if(p==null || q==null || p.val!=q.val) return false;
+
+        //call recursion for boths left and right subtree
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+     */
+}
