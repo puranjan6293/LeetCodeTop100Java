91ea8c34724d5d1fa9deea0e153b0f98760339f2
puranjan <mallik.puranjan@gmail.com>
2023-02-11 23:33:09 +0530

    in order traversal

---
+++ TOP_QUESTIONS/Tree/InOrder_Traversal.java
@@ -0,0 +1,25 @@
+package TOP_QUESTIONS.Tree;
+
+public class InOrder_Traversal {
+    /*
+    public List<Integer> inorderTraversal(TreeNode root) {
+        //Approach, Using Recursion, T.C: (n), n is the height of the tree
+        List<Integer> ans = new ArrayList<>();
+        //call recursion
+        recursion(root, ans);
+        return ans;
+
+    }
+    //recursion
+    private void recursion(TreeNode root, List<Integer> ans){
+        //base case
+        if(root==null) return;
+        //traverse left subtree
+        recursion(root.left, ans);
+        //add root.val to ans
+        ans.add(root.val);
+        //traverse right subtree
+        recursion(root.right, ans);
+    }
+    */
+}
