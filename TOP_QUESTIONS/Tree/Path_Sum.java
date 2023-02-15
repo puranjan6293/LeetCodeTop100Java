65db7ac3c4153bab8d5d6e420488f56091046b79
puranjan <mallik.puranjan@gmail.com>
2023-02-15 23:36:25 +0530

    path sum

---
+++ TOP_QUESTIONS/Tree/Path_Sum.java
@@ -0,0 +1,22 @@
+package TOP_QUESTIONS.Tree;
+
+public class Path_Sum {
+    /*
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        //Approach using path sum technique
+        //base case 
+        if(root==null) return false;
+
+        //target-root
+        targetSum = targetSum-root.val;
+
+        //case: if left and right is null
+        if(root.left==null && root.right==null){
+            return targetSum==0;
+        }
+
+        //call recursion for both left and right subteree
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+     */
+}
