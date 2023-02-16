0f071057612aed731287b8a58830a41eb2bf6561
puranjan <mallik.puranjan@gmail.com>
2023-02-16 10:48:57 +0530

    kth smallest element in bst

---
+++ TOP_QUESTIONS/Tree/kth_smallest_element_in_BST.java
@@ -0,0 +1,29 @@
+package TOP_QUESTIONS.Tree;
+
+public class kth_smallest_element_in_BST {
+    /*
+    int count = 0;
+    int ans = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        //Approach: Using dfs inorder traversal
+        inorder(root, k);
+        return ans;
+    }
+    public void inorder(TreeNode root, int k){
+        //base case 
+        if(root==null) return;
+
+        //left subtree
+        inorder(root.left, k);
+
+        //if condition satisfy
+        if(++count==k){
+            //assign the root.val to ans
+            this.ans = root.val;
+        }
+
+        //right subtree
+        inorder(root.right, k);
+    }
+     */
+}
