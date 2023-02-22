bc7569710c9e2b64f98d8a7272b214c89d9d66bb
puranjan <mallik.puranjan@gmail.com>
2023-02-22 23:35:26 +0530

    new question

---
+++ TOP_QUESTIONS/Binary_Search/Capacity_to_switch_packages_within_D_Days.java
@@ -0,0 +1,24 @@
+package TOP_QUESTIONS.Binary_Search;
+
+public class Capacity_to_switch_packages_within_D_Days {
+    public int shipWithinDays(int[] weights, int D) {
+        int left = 0, right = 0;
+        for (int w: weights) {
+            left = Math.max(left, w);
+            right += w;
+        }
+        while (left < right) {
+            int mid = (left + right) / 2, need = 1, cur = 0;
+            for (int w: weights) {
+                if (cur + w > mid) {
+                    need += 1;
+                    cur = 0;
+                }
+                cur += w;
+            }
+            if (need > D) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+}
