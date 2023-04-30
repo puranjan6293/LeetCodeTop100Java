794de7af2b139f6b093d46db5113a2eac4eb8312
puranjan <mallik.puranjan@gmail.com>
2023-04-30 10:20:36 +0530

    intersection of twoLinkedLists

---
+++ TOP_QUESTIONS/LikedQuestions/intersection_of_LinkedLists.java
@@ -0,0 +1,19 @@
+package TOP_QUESTIONS.LikedQuestions;
+
+public class intersection_of_LinkedLists {
+    /*!
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //!Approach
+        ListNode a = headA;
+        ListNode b = headB;
+
+        //!loop till they are equal
+        while(a!=b){
+            //!condition if onne of them is null
+            a = a==null? headB : a.next;
+            b = b==null? headA : b.next;
+        }
+        return a;
+    }
+     */
+}
