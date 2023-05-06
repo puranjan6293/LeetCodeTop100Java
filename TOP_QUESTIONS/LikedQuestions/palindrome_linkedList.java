3ce75d38f10901faf805a13a363727b93087ee1d
puranjan <mallik.puranjan@gmail.com>
2023-05-06 23:58:57 +0530

    palindrome Linked lIst

---
+++ TOP_QUESTIONS/LikedQuestions/palindrome_linkedList.java
@@ -0,0 +1,22 @@
+package TOP_QUESTIONS.LikedQuestions;
+public class Palindrome_LinkedList {
+    /*
+      public boolean isPalindrome(ListNode head) {
+        //Approach: Using Stack
+        Stack st = new Stack();
+        ListNode curr = head;
+        while(curr!=null){
+            st.push(curr.val);
+            curr = curr.next;
+        }
+
+        while(head!=null){
+            if(head.val!=(int)st.pop()){
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+     */
+}
