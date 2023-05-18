e14c287086f805e3bae45be61f9ab9c3e85fe75a
puranjan <mallik.puranjan@gmail.com>
2023-05-18 22:34:04 +0530

    Evaluate Reverse Polish Notation

---
+++ TOP_QUESTIONS/LikedQuestions/Evaluate_reverse_polish_notation.java
@@ -0,0 +1,25 @@
+package TOP_QUESTIONS.LikedQuestions;
+import java.util.*;
+public class Evaluate_reverse_polish_notation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                stack.add(stack.pop() + stack.pop());
+            } else if (token.equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b - a);
+            } else if (token.equals("*")) {
+                stack.add(stack.pop() * stack.pop());
+            } else if (token.equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.add(b / a);
+            } else {
+                stack.add(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
